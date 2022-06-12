981c125477a3bf95495241c90ef9ce3506fbbfb7
macanoso <macanoso@unal.edu.co>
2022-06-12 16:23:12 -0500

    Pregunta7

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    lista1 = []
+
+    for line in sys.stdin:
+        key, val1, val2 = line.split(",")
+        val2 = int(val2)
+
+        lista1.append((key, val1, val2))
+
+        lista1.sort(key=lambda i: (i[0], i[2]), reverse=False)
+
+    for elemento in lista1:
+        sys.stdout.write("{}\t{}\t{}\n".format(elemento[0], elemento[1], elemento[2]))
