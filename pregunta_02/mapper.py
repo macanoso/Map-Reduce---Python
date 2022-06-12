df01241c12b7ab4433a53c1a188c27649d3b49e4
macanoso <macanoso@unal.edu.co>
2022-06-11 20:17:25 -0500

    Pregunta 2

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        sys.stdout.write("{}\t{}\n".format(line.split(",")[3], line.split(",")[4]))
--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,30 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+import sys
+
+if __name__ == "__main__":
+
+    curkey = None
+    total = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+
+        if key == curkey:
+            if val > total:
+                total = val
+            else:
+                val = total
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\n".format(curkey, total))
+
+            curkey = key
+            total = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, total))
