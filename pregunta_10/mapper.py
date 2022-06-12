427531f489652d8b6f736a97def2234f4eb33386
macanoso <macanoso@unal.edu.co>
2022-06-12 18:12:01 -0500

    Pregunta10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        lista = line.split("\t")
+        sys.stdout.write("{}\t{}".format(lista[0], lista[1]))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+from collections import defaultdict
+import operator
+
+if __name__ == "__main__":
+
+    diccionario = defaultdict(list)
+
+    for line in sys.stdin:
+        line = line.strip("\n")
+        key, val = line.split("\t")
+        val = val.split(",")
+        key = int(key)
+
+        for i in val:
+            diccionario[i].append(key)
+
+    dct = dict(diccionario)
+    dct = dict(sorted(dct.items(), key=operator.itemgetter(0)))
+
+    for x, y in dct.items():
+        y = sorted(y, key=int)
+        y = ",".join(str(x) for x in y)
+        sys.stdout.write("{}\t{}\n".format(x, y))
