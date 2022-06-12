28b33a7ae7068371bdb836fda5800c6cff0b7cfd
macanoso <macanoso@unal.edu.co>
2022-06-12 16:09:13 -0500

    Pregunta6

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    lista1 = []
+    for line in sys.stdin:
+        line = line.strip("\n")
+        key, val = line.split(",")
+        val = float(val)
+
+        lista1.append((key, val))
+
+    d = {}
+    for x, y in lista1:
+        d.setdefault(x, []).append(y)
+
+    for x, y in d.items():
+        sys.stdout.write("{}\t{}\t{}\n".format(x, max(y), min(y)))
