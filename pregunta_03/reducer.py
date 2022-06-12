427f500fe42ca877378a1ba07811885598f55848
macanoso <macanoso@unal.edu.co>
2022-06-11 22:35:11 -0500

    Pregunta 3

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,9 +1,7 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-from email import header
 import sys
-import pandas as pd
 
 if __name__ == "__main__":
 
@@ -12,10 +10,10 @@ if __name__ == "__main__":
     for line in sys.stdin:
         key, val = line.split("\t")
         val = int(val)
-        lista.append((key, val))
-    lista.sort(key=lambda i: i[1], reverse=False)
 
-    data_output = pd.DataFrame(
-        lista,
-    )
-    print(data_output.to_csv(index=False, header=None, sep=",").rstrip("\n"))
+        lista.append((key, val))
+
+        lista.sort(key=lambda i: i[1], reverse=False)
+
+    for elemento in lista:
+        sys.stdout.write("{},{}\n".format(elemento[0], elemento[1]))
